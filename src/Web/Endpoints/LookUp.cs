c1cf862cb6411edd9be5387b9b6e36f1808c0f7f
Muhammad Hamdy <modclover7@gmail.com>
2023-12-12 23:12:39 +0200

    adding lookups

---
+++ src/Web/Endpoints/LookUp.cs
@@ -0,0 +1,20 @@
+﻿using MacClientSystem.Domain.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacClientSystem.Web.Endpoints;
+
+public class LookUp : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGet(GetBloodType, "bloodTypes");
+    }
+
+    public List<KeyValuePair<int, string?>> GetBloodType()
+    {
+        var bloodTypes = Enum.GetValues(typeof(BloodTypes)).Cast<int>()
+            .ToDictionary(value => value, value => Enum.GetName(typeof(BloodTypes), value)).ToList();
+            
+        return bloodTypes;
+    }
+}
